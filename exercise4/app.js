9fb1a0b6c2230e3cec5a523c39d86c4cc791d430
Xinwei <zxw018018@hotmail.com>
2018-05-23 00:28:57 +0800

    finish css

--- exercise4/app.js
+++ exercise4/app.js
@@ -5,7 +5,18 @@ new Vue({
   		highlight: true,
   		shrink: false
   	},
-  	float: 'float'
+  	float: 'float',
+  	userClass: '',
+  	isvisible: true,
+  	myStyle: {
+  		width: '100px',
+  		height: '150px',
+  		backgroundColor: 'grey'
+  	},
+  	progressBar: {
+  		width: '0px',
+  		backgroundColor: 'red'
+  	}
   },
   methods: {
     startEffect: function() {
@@ -14,6 +25,17 @@ new Vue({
     		vm.effectClass.highlight = !vm.effectClass.highlight;
     		vm.effectClass.shrink = !vm.effectClass.highlight;
     	}, 5000)
+    },
+    startProgress: function(){
+    	var vm = this;
+    	var width = 0;
+    	setInterval(function(){
+    		if (width<100){
+    			width = width + 10;
+    		}
+    		
+    		vm.progressBar.width = width+'px';
+    	}, 500)
     }
   }
 });
