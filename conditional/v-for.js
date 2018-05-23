25ce770e1717d17d7937a284ec59c18c85b1226b
Xinwei <zxw018018@hotmail.com>
2018-05-23 12:17:49 +0800

    before monster project

---
+++ conditional/v-for.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: "#app",
+	data: {
+		ingredients: ["meat", "fruit", "cookies"],
+		persons: [
+			{name: "Max", age: 24, color: "red"},
+			{name: "Bill", age: 25, color: "yellow"}
+		]
+	}
+});
