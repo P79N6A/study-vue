a64ec3c8a39bd9ecab70542a1442a2dbc691cf6e
Xinwei <zxw018018@hotmail.com>
2018-05-28 23:07:31 +0800

    start vue-resource

--- filter/src/App.vue
+++ filter/src/App.vue
@@ -5,6 +5,7 @@
                 <h1>Filters & Mixins</h1>
                 <p>{{text | toUppercase | to-lowercase}}</p>
                 <hr>
+                <button @click="fruits.push('Berries')">Add New Item</button>
                 <input v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits">{{fruit}}</li>
@@ -19,25 +20,17 @@
 
 <script>
     import List from './List.vue';
+    import { fruitMixin } from './fruitMixin';
     export default {
+        mixins: [fruitMixin],
         data() {
             return {
                 text: "Hello there!",
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
             }
         },
         filters: {
             toUppercase(value){
                 return value.toUpperCase();
-            }
-        },
-        computed: {
-            filteredFruits(){
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                    }
-                );
             }
         },
         components: {
--- filter/src/List.vue
+++ filter/src/List.vue
@@ -9,20 +9,11 @@
 </template>
 
 <script>
+    import { fruitMixin } from './fruitMixin';
     export default {
-        data() {
-            return {
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
-            }
-        },
-        computed: {
-            filteredFruits(){
-                return this.fruits.filter((element) => {
-                        return element.match(this.filterText);
-                    }
-                );
-            }
+        mixins: [fruitMixin],
+        created(){
+            console.log("inside list created hook");
         }
     }
 </script>
