eb5e284accf3ca8a25146c26de8c555e3db05bb4
Xinwei <zxw018018@hotmail.com>
2018-05-29 21:23:33 +0800

    vuex

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -12,6 +12,13 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <input class="form-control" type="text" v-model="node">
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,18 +31,56 @@
                 user: {
                     username: '',
                     email: ''
-                }
+                },
+                users: [],
+                resource: {},
+                node: 'data'
             }
         },
         methods: {
             submit(){
-                this.$http.post('https://vuejs-http-365a9.firebaseio.com/data.json', this.user).then(response=>{
-                    console.log(response);
-                }, error => {
-                    console.log(error);
-                });
+                // this.$http.post('data.json', this.user).then(response=>{
+                //     console.log(response);
+                // }, error => {
+                //     console.log(error);
+                // });
+                // this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
+            },
+            fetchData(){
+                // this.$http.get('data.json')
+                //     .then(response=>{
+                //         return response.json();
+                //     })
+                //     .then(data => {
+                //         const resultArray = [];
+                //         for (let key in data){
+                //             resultArray.push(data[key]);
+                //         }
+                //         this.users = resultArray;
+                //     });
+                this.resource.getData({node: this.node})
+                    .then(response=>{
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data){
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    });
             }
+        },
+        created(){
+            const customActions = {
+              saveAlt: {method: 'POST',
+                        url: 'alternative.json'},
+                getData: {method: 'GET'}
+            };
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
+
     }
 </script>
 
--- vue-resource/src/main.js
+++ vue-resource/src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-http-365a9.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+      response.json = () => { return {messages: response.body}}
+  });
+
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
